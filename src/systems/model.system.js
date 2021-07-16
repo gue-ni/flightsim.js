407aa9f7417e9e225cb2327d12d7fb5f515659c3
Jakob <jakob.g.maier@gmail.com>
2021-07-16 17:27:08 +0200

    update to model

--- src/systems/model.system.js
+++ src/systems/model.system.js
@@ -1,16 +1,18 @@
 import * as THREE from "three";
 import * as ECS from "lofi-ecs";
 import { FalconModel } from "../components/model.component";
-import { CockpitView, OrbitView, ViewComponent } from "../components/view.component";
+import { CockpitView, ViewComponent } from "../components/view.component";
+import { Joystick } from "../components/joystick.component";
 
 export class ModelSystem extends ECS.System {
 	constructor() {
-		super([FalconModel, ViewComponent]);
+		super([FalconModel, ViewComponent, Joystick]);
 	}
 
 	updateEntity(entity, dt, params) {
 		const view = entity.getComponent(ViewComponent).current;
 		const model = entity.getComponent(FalconModel);
+		const joystick = entity.getComponent(Joystick);
 
 		if (view.constructor == CockpitView) {
 			model.pilot.visible = false;
@@ -18,6 +20,21 @@ export class ModelSystem extends ECS.System {
 		} else {
 			model.pilot.visible = true;
 			model.cannopy.visible = true;
+		}
+
+		let m;
+
+		m = 0.5;
+		if (joystick.pitch) {
+			const el = -joystick.pitch * 0.5;
+			model.elevator.rotation.set(el, 0, 0);
+		}
+
+		m = 0.1;
+		if (joystick.roll) {
+			const fl = THREE.MathUtils.clamp(-Math.PI * joystick.roll, -m, m);
+			model.leftflap.rotation.set(fl, 0, 0);
+			model.rightflap.rotation.set(-fl, 0, 0);
 		}
 	}
 }
